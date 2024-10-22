3594fba4fce1bd00dd4384cd09b6d8412147d018
zachysaur <124980322+zachysaur@users.noreply.github.com>
2024-10-22 17:35:50 +0500

    Update inference.py

--- inference.py
+++ inference.py
@@ -1,4 +1,6 @@
 import datetime
+import os
+import shutil
 from omegaconf import OmegaConf
 from argparse import ArgumentParser
 
@@ -12,54 +14,62 @@ from src.modules.file_operations import FileOperations
 def main(args):
     cfg = OmegaConf.load(args.config_path)
     cfg = cfg.inference
-    print("Config File is loaded succesfully!")
+    print("Config File is loaded successfully!")
+
+    # Create output directory if it doesn't exist
+    if not os.path.exists(args.save_path):
+        os.makedirs(args.save_path)
 
     file_operations = FileOperations(device=cfg.device,
                                      save_path=args.save_path,
                                      source_path=args.source_path,
                                      audio_path=args.audio_path,
                                      ref_head_pose_path=args.ref_head_pose_path)
-    
+
     preprocess = Preprocess(device=cfg.device,
                             fps=cfg.fps,
                             sadtalker_checkpoint_path=cfg.sadtalker_checkpoint_path,
                             preprocessed_inputs_exist=file_operations.preprocessed_inputs_exist,
                             ref_head_pose_inputs_exist=file_operations.ref_head_pose_inputs_exist,
                             **cfg.preprocess)
-    
+
     audio2coeff = SadtalkerAudio2Coeff(device=cfg.device,
                                        sadtalker_checkpoint_path=cfg.sadtalker_checkpoint_path,
                                        **cfg.audio2coeff)
-    
+
     map2lp = Map2LivePortrait(device=cfg.device,
                               **cfg.map2lp)
-    
+
     lp_render = LivePortraitRender(device=cfg.device,
                                    fps=cfg.fps,
                                    instant_save_func=file_operations.instant_save_input,
                                    **cfg.lp_render)
-    
+
     print("Pipeline Objects are initialized!")
-    
-    batch = {"source_path": args.source_path,
-             "audio_path": args.audio_path,
-             "still": args.still,
-             "pupil_x": args.pupil_x,
-             "pupil_y": args.pupil_y,
-             "no_crop": args.no_crop,
-             "ref_head_pose_path": args.ref_head_pose_path,
-             "ref_frames_from_zero": args.ref_frames_from_zero,
-             "time": datetime.datetime.now().strftime("%d%m%Y-%H%M%S")}
-    
+
+    batch = {
+        "source_path": args.source_path,
+        "audio_path": args.audio_path,
+        "still": args.still,
+        "pupil_x": args.pupil_x,
+        "pupil_y": args.pupil_y,
+        "no_crop": args.no_crop,
+        "ref_head_pose_path": args.ref_head_pose_path,
+        "ref_frames_from_zero": args.ref_frames_from_zero,
+        "time": datetime.datetime.now().strftime("%d%m%Y-%H%M%S")
+    }
+
     if file_operations.preprocessed_inputs_exist or file_operations.ref_head_pose_inputs_exist:
         batch = {**batch, **file_operations.load_inputs()}  
 
     pipeline = [preprocess, audio2coeff, map2lp, lp_render]
+    
     for pipe_func in pipeline:
         batch = pipe_func(batch)
 
     file_operations.save(batch=batch)
     print("Done")
+
 
 if __name__ == "__main__":
     parser = ArgumentParser() 
@@ -73,6 +83,7 @@ if __name__ == "__main__":
     parser.add_argument("--pupil_y", type=float, default=0., help="pupil retargeting value in y axis")
     parser.add_argument("--ref_head_pose_path", type=str, default=None, help="path to reference head pose")
     parser.add_argument("--ref_frames_from_zero", action="store_true", help="starts reference head pose from beginning instead of random selection")
+    
     args = parser.parse_args()
 
     main(args)
